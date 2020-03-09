f1beb6e74d5588ebac018d95ff1bfc5e9cb103c4
jiangzaohong <red@jollycorp.com>
2020-03-09 15:29:47 +0800

    test

---
+++ src/test/java/DemoTest/JustTest.java
@@ -0,0 +1,12 @@
+package DemoTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class JustTest {
+
+    @Test
+    public void test(){
+        Assert.assertEquals(1,2);
+    }
+}
